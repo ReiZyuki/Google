bf058f3a440ae18345f2833d7a1275f3c0d5ae77
ReiZyuki <azadofficial2026@gmail.com>
2026-09-13 15:11:15 +0530

    Build utils

---
+++ utils.py
@@ -0,0 +1,26 @@
+import keyword
+import inspect
+
+def set_result(target, value):
+    if not isinstance(target, str): raise TypeError("Result variable name must be a string.")
+    return value
+
+def validate_target(target):
+    if not isinstance(target, str): raise TypeError("Right-side value must be a variable name string.")
+    if not target.isidentifier() or keyword.iskeyword(target):
+        raise ValueError(f"Invalid Python variable name: {target}")
+
+def inject_result(target, value, library_dir):
+    frame = inspect.currentframe()
+    try:
+        frame = frame.f_back
+        while frame:
+            filename = frame.f_code.co_filename
+            if not filename.startswith(library_dir):
+                frame.f_globals[target] = value
+                try: frame.f_locals[target] = value
+                except Exception: pass
+                break
+            frame = frame.f_back
+    finally:
+        del frame
